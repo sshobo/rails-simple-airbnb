d0caae470b666159328bf9f3e5799e0fd6ad5325
Samuel Shobo <samuelshobo06@gmail.com>
2025-01-09 20:54:54 +0000

    controller index minor-styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Flat.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating flats..."
+
+4.times do
+  Flat.create!(
+    name: Faker::Locations::Australia.state,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(1..6)
+  )
+end
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Flat.count} flats."
