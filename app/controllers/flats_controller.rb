ef3d99c3e20c06d6240ea5a15fd0951bd50ac387
Samuel Shobo <samuelshobo06@gmail.com>
2025-01-10 16:45:58 +0000

    minor styling show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,4 +6,18 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to @flat
+  end
+
+  def show
+  end
+
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
